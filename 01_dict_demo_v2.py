4ca369a339ec07543fb652664496164084f97e61
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2022-10-28 11:05:04 +1300

    Add files via upload

---
+++ 01_dict_demo_v2.py
@@ -0,0 +1,18 @@
+my_dict = {
+    "blue": "sky", 
+    "double": 2,
+    "half": 0.5,
+    "green": "grass",
+    "yellow": "sun",
+    "red": "apple"
+}
+
+your_num = int(input("Enter a number: "))
+to_do = input("double or half? ").lower()
+
+# look up value
+multiply_by = my_dict[to_do]
+
+# do math!
+answer = your_num * multiply_by
+print("{} x {} = {}".format(your_num, multiply_by, answer))
